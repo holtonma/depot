4e28d5ebf1de5133b397526f7babe6f16272a721
Mark Holton <holtonma@Macintosh-69.local>
2009-02-07 17:16:37 -0800

    put find_cart into store_controller, so it will create the session variable if it's not already there... otherwise if using the module, couldn't figure out how to create session in lib dir

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,15 +3,21 @@ include StoreUtil
 class StoreController < ApplicationController
   
   def index
-    StoreUtil.find_cart(session[:cart])
+    find_cart 
     @products = Product.find_products_for_sale
     @cart_id = 1 # for now, no users
   end
   
   def add_to_cart
-    @cart = StoreUtil.find_cart(session[:cart]) 
+    @cart = find_cart 
     product = Product.find(params[:id])
     @cart.add_product(product)
   end
+  
+  private
+    def find_cart
+      session[:cart] ||= Cart.new
+    end
+    
 
 end
