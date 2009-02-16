add65bee13bd0330afda3922573fb80f5f95ee0e
Mark Holton <holtonma@Macintosh-69.local>
2009-02-15 16:25:10 -0800

    adding more user controller tests

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -27,7 +27,53 @@ class UsersControllerTest < ActionController::TestCase
  
     assert_redirected_to users_path
   end
- 
+  
+  test "should not create user and show red box when name is blank" do
+    assert_no_difference('User.count') do
+      post :create, :user => {
+        :name                   => '',
+        :password               => 'onetwo',
+        :password_confirmation  => 'onetwo',
+      }
+    end
+    
+    assert_response :ok
+    assert_tag :tag => "div", :attributes => { :id => "errorExplanation" }
+    assert_tag :tag => "div", :attributes => { :class => "fieldWithErrors"}
+    assert_select "div[class*=fieldWithErrors]", :count => 2
+    assert_select "div[class*=errorExplanation]", :count => 1
+    # check explicitly that both the input is blank for Title and it is a fieldWithErrors
+    assert_select "div.fieldWithErrors" do 
+      assert_select "input:last-of-type", ""
+    end
+    # check explicitly that both the input#product_price errs out and is blank
+    assert_equal 1, css_select("div.fieldWithErrors > input#user_name").size 
+    assert_select "div.fieldWithErrors > input#user_name", :text => ""
+  end
+  
+  test "should not create user and show red box when password is blank" do
+    assert_no_difference('User.count') do
+      post :create, :user => {
+        :name                   => 'asdfasdfasdf',
+        :password               => '',
+        :password_confirmation  => 'onetwo',
+      }
+    end
+    
+    assert_response :ok
+    assert_tag :tag => "div", :attributes => { :id => "errorExplanation" }
+    assert_tag :tag => "div", :attributes => { :class => "fieldWithErrors"}
+    assert_select "div[class*=fieldWithErrors]", :count => 1
+    assert_select "div[class*=errorExplanation]", :count => 1
+    # check explicitly that both the input is blank for Title and it is a fieldWithErrors
+    assert_select "div.fieldWithErrors" do 
+      assert_select "input:last-of-type", ""
+    end
+    # check explicitly that both the input#product_price errs out and is blank
+    assert_equal 1, css_select("div.fieldWithErrors > input#user_password").size 
+    assert_select "div.fieldWithErrors > input#user_password", :text => ""
+  end
+  
   test "should show user" do
     get :show, :id => users(:one).id
     assert_response :success
