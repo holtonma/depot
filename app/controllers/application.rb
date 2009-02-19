f3f0c56e59e7c1adcf04e08457ca53ef32accc38
Mark Holton <holtonma@Macintosh-69.local>
2009-02-18 23:07:27 -0800

    continuing with internationalization: yaml files and application updates

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   layout "store" 
   
   before_filter :authorize, :except => :login
+  before_filter :set_locale 
+  
   helper :all # include all helpers, all the time
 
   # See ActionController::RequestForgeryProtection for details
@@ -22,6 +24,25 @@ class ApplicationController < ActionController::Base
         flash[:notice] = "Please log in" 
         redirect_to :controller => 'admin', :action => 'login' 
       end 
+    end 
+  
+    def set_locale 
+      session[:locale] = params[:locale] if params[:locale] 
+      I18n.locale = session[:locale] || I18n.default_locale 
+      
+      locale_path = "#{LOCALES_DIRECTORY}#{I18n.locale}.yml" 
+      
+      unless I18n.load_path.include? locale_path 
+        I18n.load_path << locale_path 
+        I18n.backend.send(:init_translations) 
+      end 
+      
+    rescue Exception => err 
+      logger.error err 
+      flash.now[:notice] = "#{I18n.locale} translation not available" 
+      
+      I18n.load_path -= [locale_path] 
+      I18n.locale = session[:locale] = I18n.default_locale 
     end 
      
 end
